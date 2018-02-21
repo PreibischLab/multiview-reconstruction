19b0004c60da31978d3384ee946153a6376ef217
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-02-21 12:48:11 +0100

    fix crash when there are too few interest points

--- src/main/java/net/preibisch/mvrecon/process/interestpointregistration/pairwise/methods/geometrichashing/GeometricHashingPairwise.java
+++ src/main/java/net/preibisch/mvrecon/process/interestpointregistration/pairwise/methods/geometrichashing/GeometricHashingPairwise.java
@@ -62,7 +62,9 @@ public class GeometricHashingPairwise< I extends InterestPoint > implements Matc
 		for ( final I i : listBIn )
 			listB.add( i );
 
-		if ( listA.size() < 4 || listB.size() < 4 )
+		final int minPoints = 3 + gp.getRedundancy() + 1;
+
+		if ( listA.size() < minPoints || listB.size() < minPoints )
 		{
 			result.setResult( System.currentTimeMillis(), "Not enough detections to match" );
 			result.setCandidates( new ArrayList< PointMatchGeneric< I > >() );
