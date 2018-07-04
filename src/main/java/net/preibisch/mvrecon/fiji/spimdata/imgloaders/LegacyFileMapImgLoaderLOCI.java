PreibischLab/multiview-reconstruction

commit 11420414a9a2f54b7e941fd2387b1983ec140464

some generics cleanup, file map now uses ViewIds as keys.

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/LegacyFileMapImgLoaderLOCI.java
@@ -48,6 +48,7 @@ import mpicbg.spim.data.sequence.Illumination;
 import mpicbg.spim.data.sequence.ImgLoader;
 import mpicbg.spim.data.sequence.Tile;
 import mpicbg.spim.data.sequence.TimePoint;
+import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.data.sequence.ViewSetup;
 import mpicbg.spim.io.IOFunctions;
@@ -73,20 +74,21 @@ import net.preibisch.mvrecon.fiji.spimdata.imgloaders.filemap2.FileMapGettable;
 public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 {
 	
-	private HashMap<BasicViewDescription< ? >, Pair<File, Pair<Integer, Integer>>> fileMap;
+	private HashMap<ViewId, Pair<File, Pair<Integer, Integer>>> fileMap;
 	private AbstractSequenceDescription< ?, ?, ? > sd;
-	private IFormatReader reader;
 	private boolean allTimepointsInSingleFiles;
 
 	public LegacyFileMapImgLoaderLOCI(
-			HashMap<BasicViewDescription< ? >, Pair<File, Pair<Integer, Integer>>> fileMap,
+			Map<? extends ViewId, Pair<File, Pair<Integer, Integer>>> fileMap,
 			final ImgFactory< ? extends NativeType< ? > > imgFactory,
 			final AbstractSequenceDescription<?, ?, ?> sequenceDescription )
 	{
 		super();
-		this.fileMap = fileMap;
+
+		this.fileMap = new HashMap<>();
+		this.fileMap.putAll( fileMap );
+
 		this.sd = sequenceDescription;
-		this.reader = new ImageReader();
 
 		setImgFactory( imgFactory );
 
@@ -94,14 +96,14 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 		
 		// populate map file -> {time points}
 		Map< File, Set< Integer > > tpsPerFile = new HashMap<>();
-		for ( BasicViewDescription< ? > vd : fileMap.keySet() )
+		for ( ViewId vId : fileMap.keySet() )
 		{
 
-			final File fileForVd = fileMap.get( vd ).getA();
+			final File fileForVd = fileMap.get( vId ).getA();
 			if ( !tpsPerFile.containsKey( fileForVd ) )
 				tpsPerFile.put( fileForVd, new HashSet<>() );
 
-			tpsPerFile.get( fileForVd ).add( vd.getTimePointId() );
+			tpsPerFile.get( fileForVd ).add( vId.getTimePointId() );
 
 			// the current file has more than one time point
 			if ( tpsPerFile.get( fileForVd ).size() > 1 )
@@ -359,7 +361,7 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 	}
 
 
-	public HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > getFileMap()
+	public Map< ViewId, Pair< File, Pair< Integer, Integer > > > getFileMap()
 	{
 		return fileMap;
 	}
@@ -373,11 +375,12 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 	public static boolean isZSizeEqualInEveryFile(final SpimData2 spimData, final FileMapGettable loader)
 	{
 		// for every file: collect a vd for every series
-		final Map< File, Map< Integer, BasicViewDescription< ? > > > invertedMap = new HashMap<>();
+		final Map< File, Map< Integer, ViewId > > invertedMap = new HashMap<>();
 		loader.getFileMap().entrySet().forEach( e -> {
 
+			final ViewDescription vd = spimData.getSequenceDescription().getViewDescription( e.getKey() );
 			// ignore missing views
-			if (!e.getKey().isPresent())
+			if (!vd.isPresent())
 				return;
 
 			final File invKey = e.getValue().getA();
@@ -385,21 +388,21 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 				invertedMap.put( invKey, new HashMap<>() );
 
 			final Integer series = e.getValue().getB().getA();
-			final BasicViewDescription< ? > vd = e.getKey();
 
 			invertedMap.get( invKey ).put( series, vd );
 		});
 
 		// filter all files that do no have equal z-size for all series
-		final List< Entry< File, Map< Integer, BasicViewDescription< ? > > > > mapFiltered = invertedMap.entrySet().stream().filter( e -> {
-			final Map< Integer, BasicViewDescription< ? > > seriesMap = e.getValue();
+		final List< Entry< File, Map< Integer, ViewId > > > mapFiltered = invertedMap.entrySet().stream().filter( e -> {
+			final Map< Integer, ViewId > seriesMap = e.getValue();
 
 			Long zSize = null;
 			for (Integer series : seriesMap.keySet())
 			{
+				final ViewDescription vd = spimData.getSequenceDescription().getViewDescription( seriesMap.get( series ) );
 				if (zSize == null)
-					zSize = seriesMap.get( series ).getViewSetup().getSize().dimension( 2 );
-				if (zSize != seriesMap.get( series ).getViewSetup().getSize().dimension( 2 ))
+					zSize = vd.getViewSetup().getSize().dimension( 2 );
+				if (zSize != vd.getViewSetup().getSize().dimension( 2 ))
 					return false;
 			}
 			return true;
@@ -424,20 +427,20 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 		final Map< Pair< File, Integer >, List< BasicViewDescription< ViewSetup > > > invertedMap = new HashMap<>();
 		loader.getFileMap().entrySet().forEach( e -> {
 
+			final ViewDescription vd = spimData.getSequenceDescription().getViewDescription( e.getKey() );
+
 			// ignore if we cannot cast to ViewSetup
-			if (!ViewSetup.class.isInstance( e.getKey().getViewSetup() ) )
+			if (!ViewSetup.class.isInstance( vd.getViewSetup() ) )
 				return;
 
 			// ignore missing views
-			if (!e.getKey().isPresent())
+			if (vd.isPresent())
 				return;
 
 			final Pair< File, Integer > invKey = new ValuePair<>( e.getValue().getA(), e.getValue().getB().getA() );
 			if (!invertedMap.containsKey( invKey ))
 				invertedMap.put( invKey, new ArrayList<>() );
 
-			@SuppressWarnings("unchecked")
-			final BasicViewDescription< ViewSetup > vd = (BasicViewDescription< ViewSetup >) e.getKey();
 			invertedMap.get( invKey ).add( vd );
 		});
 

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/FileMapImgLoaderLOCI.java
@@ -23,11 +23,11 @@
 package net.preibisch.mvrecon.fiji.spimdata.imgloaders;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.Map;
 
 import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
-import mpicbg.spim.data.generic.sequence.BasicViewDescription;
 import mpicbg.spim.data.legacy.LegacyImgLoaderWrapper;
+import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.img.ImgFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
@@ -38,7 +38,7 @@ public class FileMapImgLoaderLOCI extends LegacyImgLoaderWrapper< UnsignedShortT
 {
 
 	public FileMapImgLoaderLOCI(
-			HashMap<BasicViewDescription< ? >, Pair<File, Pair<Integer, Integer>>> fileMap,
+			Map<? extends ViewId, Pair<File, Pair<Integer, Integer>>> fileMap,
 			final ImgFactory< ? extends NativeType< ? > > imgFactory,
 			final AbstractSequenceDescription<?, ?, ?> sequenceDescription )
 	{
@@ -54,7 +54,7 @@ public class FileMapImgLoaderLOCI extends LegacyImgLoaderWrapper< UnsignedShortT
 	}
 	
 	@Override
-	public HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > getFileMap()
+	public Map< ViewId, Pair< File, Pair< Integer, Integer > > > getFileMap()
 	{
 		 return ( (LegacyFileMapImgLoaderLOCI) legacyImgLoader ).getFileMap();
 	}

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/XmlIoFileListImgLoaderLOCI.java
@@ -27,6 +27,7 @@ import static mpicbg.spim.data.XmlKeys.IMGLOADER_FORMAT_ATTRIBUTE_NAME;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.jdom2.Element;
 
@@ -61,7 +62,7 @@ public class XmlIoFileListImgLoaderLOCI implements XmlIoBasicImgLoader< FileMapI
 	@Override
 	public Element toXml(FileMapImgLoaderLOCI imgLoader, File basePath)
 	{
-		HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > fileMap = imgLoader.getFileMap();
+		Map< ViewId, Pair< File, Pair< Integer, Integer > > > fileMap = imgLoader.getFileMap();
 		
 		final Element wholeElem = new Element( "ImageLoader" );
 		wholeElem.setAttribute( IMGLOADER_FORMAT_ATTRIBUTE_NAME, this.getClass().getAnnotation( ImgLoaderIo.class ).format() );
@@ -69,12 +70,12 @@ public class XmlIoFileListImgLoaderLOCI implements XmlIoBasicImgLoader< FileMapI
 		
 		final Element filesElement = new Element( FILES_TAG );
 		
-		for (BasicViewDescription< ? > vs : fileMap.keySet())
+		for (ViewId vid : fileMap.keySet())
 		{
-			final Pair< File, Pair< Integer, Integer > > pair = fileMap.get( vs );
+			final Pair< File, Pair< Integer, Integer > > pair = fileMap.get( vid );
 			final Element fileMappingElement = new Element( FILE_MAPPING_TAG );
-			fileMappingElement.setAttribute( MAPPING_VS_TAG, Integer.toString( vs.getViewSetupId()) );
-			fileMappingElement.setAttribute( MAPPING_TP_TAG, Integer.toString( vs.getTimePointId()) );
+			fileMappingElement.setAttribute( MAPPING_VS_TAG, Integer.toString( vid.getViewSetupId()) );
+			fileMappingElement.setAttribute( MAPPING_TP_TAG, Integer.toString( vid.getTimePointId()) );
 			fileMappingElement.addContent( XmlHelpers.pathElement( MAPPING_FILE_TAG, pair.getA(), basePath ) );
 			fileMappingElement.setAttribute( MAPPING_SERIES_TAG, Integer.toString(pair.getB().getA()) );
 			fileMappingElement.setAttribute( MAPPING_C_TAG, Integer.toString(pair.getB().getB()) );

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/filemap2/FileMapGettable.java
@@ -23,14 +23,12 @@
 package net.preibisch.mvrecon.fiji.spimdata.imgloaders.filemap2;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.Map;
 
-import mpicbg.spim.data.generic.sequence.BasicViewDescription;
+import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.util.Pair;
 
 public interface FileMapGettable
 {
-
-	HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > getFileMap();
-
+	public Map< ? extends ViewId, Pair< File, Pair< Integer, Integer > > > getFileMap();
 }

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/filemap2/FileMapImgLoaderLOCI2.java
@@ -52,44 +52,39 @@ import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.ShortType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
-import net.imglib2.type.numeric.integer.UnsignedIntType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Pair;
 import net.imglib2.view.Views;
 import net.preibisch.mvrecon.fiji.spimdata.imgloaders.AbstractImgLoader;
-import net.preibisch.mvrecon.fiji.spimdata.imgloaders.LegacyFileMapImgLoaderLOCI;
 
 public class FileMapImgLoaderLOCI2 implements ImgLoader, FileMapGettable
 {
-	private final HashMap<BasicViewDescription< ? >, Pair<File, Pair<Integer, Integer>>> fileMap;
+	private final HashMap<ViewId, Pair<File, Pair<Integer, Integer>>> fileMap;
 	private final AbstractSequenceDescription<?, ?, ?> sd;
 	private boolean allTimepointsInSingleFiles;
-	private final Map< File, IFormatReader > readers;
 	
-	public FileMapImgLoaderLOCI2(HashMap<BasicViewDescription< ? >, Pair<File, Pair<Integer, Integer>>> fileMap,
+	public FileMapImgLoaderLOCI2(Map<? extends ViewId, Pair<File, Pair<Integer, Integer>>> fileMap,
 			final ImgFactory< ? extends NativeType< ? > > imgFactory, // FIXME: remove this, only here to test quick replacement
 			final AbstractSequenceDescription<?, ?, ?> sequenceDescription)
 	{
-		this.fileMap = fileMap;
+		this.fileMap = new HashMap<>();
+		this.fileMap.putAll( fileMap );
+
 		this.sd = sequenceDescription;
-		
-		this.readers = new HashMap<>();
-		
+
 		allTimepointsInSingleFiles = true;
 		
 		// populate map file -> {time points}
 		Map< File, Set< Integer > > tpsPerFile = new HashMap<>();
-		for ( BasicViewDescription< ? > vd : fileMap.keySet() )
+		for ( ViewId vid : fileMap.keySet() )
 		{
 
-			final File fileForVd = fileMap.get( vd ).getA();
+			final File fileForVd = fileMap.get( vid ).getA();
 			if ( !tpsPerFile.containsKey( fileForVd ) )
 				tpsPerFile.put( fileForVd, new HashSet<>() );
 
-			tpsPerFile.get( fileForVd ).add( vd.getTimePointId() );
+			tpsPerFile.get( fileForVd ).add( vid.getTimePointId() );
 
 			// the current file has more than one time point
 			if ( tpsPerFile.get( fileForVd ).size() > 1 )
@@ -115,7 +110,7 @@ public class FileMapImgLoaderLOCI2 implements ImgLoader, FileMapGettable
 	 * @see spim.fiji.spimdata.imgloaders.filemap2.FileMapGettable#getFileMap()
 	 */
 	@Override
-	public HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > getFileMap()
+	public Map< ViewId, Pair< File, Pair< Integer, Integer > > > getFileMap()
 	{
 		 return fileMap;
 	}

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/filemap2/XmlIOFileMapImgLoaderLOCI2.java
@@ -26,6 +26,7 @@ import static mpicbg.spim.data.XmlKeys.IMGLOADER_FORMAT_ATTRIBUTE_NAME;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.jdom2.Element;
 
@@ -55,7 +56,7 @@ public class XmlIOFileMapImgLoaderLOCI2 implements XmlIoBasicImgLoader< FileMapI
 	@Override
 	public Element toXml(FileMapImgLoaderLOCI2 imgLoader, File basePath)
 	{
-		HashMap< BasicViewDescription< ? >, Pair< File, Pair< Integer, Integer > > > fileMap = imgLoader.getFileMap();
+		Map< ViewId, Pair< File, Pair< Integer, Integer > > > fileMap = imgLoader.getFileMap();
 
 		final Element wholeElem = new Element( "ImageLoader" );
 		wholeElem.setAttribute( IMGLOADER_FORMAT_ATTRIBUTE_NAME,
@@ -63,12 +64,12 @@ public class XmlIOFileMapImgLoaderLOCI2 implements XmlIoBasicImgLoader< FileMapI
 
 		final Element filesElement = new Element( FILES_TAG );
 
-		for ( BasicViewDescription< ? > vs : fileMap.keySet() )
+		for ( ViewId vid : fileMap.keySet() )
 		{
-			final Pair< File, Pair< Integer, Integer > > pair = fileMap.get( vs );
+			final Pair< File, Pair< Integer, Integer > > pair = fileMap.get( vid );
 			final Element fileMappingElement = new Element( FILE_MAPPING_TAG );
-			fileMappingElement.setAttribute( MAPPING_VS_TAG, Integer.toString( vs.getViewSetupId() ) );
-			fileMappingElement.setAttribute( MAPPING_TP_TAG, Integer.toString( vs.getTimePointId() ) );
+			fileMappingElement.setAttribute( MAPPING_VS_TAG, Integer.toString( vid.getViewSetupId() ) );
+			fileMappingElement.setAttribute( MAPPING_TP_TAG, Integer.toString( vid.getTimePointId() ) );
 			fileMappingElement.addContent( XmlHelpers.pathElement( MAPPING_FILE_TAG, pair.getA(), basePath ) );
 			fileMappingElement.setAttribute( MAPPING_SERIES_TAG, Integer.toString( pair.getB().getA() ) );
 			fileMappingElement.setAttribute( MAPPING_C_TAG, Integer.toString( pair.getB().getB() ) );

commit c5ce4c27e0fceb424b241248f5878886a20af4e7

fix bug of stack size check being skipped. was due to typo introduced by 11420414a9a2f54b7e941fd2387b1983ec140464

src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/LegacyFileMapImgLoaderLOCI.java
@@ -428,7 +428,7 @@ public class LegacyFileMapImgLoaderLOCI extends AbstractImgFactoryImgLoader
 				return;
 
 			// ignore missing views
-			if (vd.isPresent())
+			if (!vd.isPresent())
 				return;
 
 			final Pair< File, Integer > invKey = new ValuePair<>( e.getValue().getA(), e.getValue().getB().getA() );