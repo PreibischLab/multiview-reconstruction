6f5a475a12193b25466ecc14747f8720e2c6adc1
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-07-18 18:27:08 +0200

    unified two Transformation util classes

--- src/main/java/net/preibisch/mvrecon/process/interestpointregistration/TransformationTools.java
+++ src/main/java/net/preibisch/mvrecon/process/interestpointregistration/TransformationTools.java
@@ -68,6 +68,7 @@ import net.preibisch.mvrecon.fiji.spimdata.interestpoints.InterestPointList;
 import net.preibisch.mvrecon.fiji.spimdata.interestpoints.ViewInterestPointLists;
 import net.preibisch.mvrecon.process.boundingbox.BoundingBoxMaximal;
 import net.preibisch.mvrecon.process.interestpointregistration.pairwise.constellation.grouping.Group;
+import net.preibisch.mvrecon.vecmath.Matrix4d;
 import net.preibisch.mvrecon.vecmath.Matrix4f;
 import net.preibisch.mvrecon.vecmath.Quat4f;
 import net.preibisch.mvrecon.vecmath.Transform3D;
@@ -263,6 +264,7 @@ public class TransformationTools
 
 	public static String getScaling( final Transform3D t )
 	{
+		
 		final Vector3d v = new Vector3d();
 		t.getScale( v );
 		return "Scaling: " + f.format( v.x ) + ", " + f.format( v.y ) + ", " + f.format( v.z );
@@ -348,6 +350,82 @@ public class TransformationTools
 		transform.set( m2 );
 
 		return transform;
+	}
+
+	public static AffineModel3D getAffineModel3D( Transform3D transform )
+	{
+		final double[] m = new double[16];
+		transform.get( m );
+
+		AffineModel3D model = new AffineModel3D();
+		model.set( m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11] );
+
+		return model;
+	}
+
+	public static RigidModel3D getRigidModel3D( Transform3D transform )
+	{
+		final double[] m = new double[16];
+		transform.get( m );
+
+		RigidModel3D model = new RigidModel3D();
+		model.set( m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11] );
+
+		return model;
+	}
+
+	public static Matrix4d getMatrix4d( final AffineModel3D model )
+	{
+		final Matrix4d matrix = new Matrix4d();
+
+		final double[] m = new double[ 12 ];
+		model.getMatrix( m );
+
+		matrix.m00 = m[ 0 ];
+		matrix.m01 = m[ 1 ];
+		matrix.m02 = m[ 2 ];
+		matrix.m03 = m[ 3 ];
+		matrix.m10 = m[ 4 ];
+		matrix.m11 = m[ 5 ];
+		matrix.m12 = m[ 6 ];
+		matrix.m13 = m[ 7 ];
+		matrix.m20 = m[ 8 ];
+		matrix.m21 = m[ 9 ];
+		matrix.m22 = m[ 10 ];
+		matrix.m23 = m[ 11 ];
+		matrix.m30 = 0;
+		matrix.m31 = 0;
+		matrix.m32 = 0;
+		matrix.m33 = 0;
+
+		return matrix;
+	}
+
+	public static Matrix4d getMatrix4d( final RigidModel3D model )
+	{
+		final Matrix4d matrix = new Matrix4d();
+
+		final double[] m = new double[ 12 ];
+		model.getMatrix( m );
+
+		matrix.m00 = m[ 0 ];
+		matrix.m01 = m[ 1 ];
+		matrix.m02 = m[ 2 ];
+		matrix.m03 = m[ 3 ];
+		matrix.m10 = m[ 4 ];
+		matrix.m11 = m[ 5 ];
+		matrix.m12 = m[ 6 ];
+		matrix.m13 = m[ 7 ];
+		matrix.m20 = m[ 8 ];
+		matrix.m21 = m[ 9 ];
+		matrix.m22 = m[ 10 ];
+		matrix.m23 = m[ 11 ];
+		matrix.m30 = 0;
+		matrix.m31 = 0;
+		matrix.m32 = 0;
+		matrix.m33 = 0;
+
+		return matrix;
 	}
 
 	public static String printAffine3D( final Affine3D< ? > model )
