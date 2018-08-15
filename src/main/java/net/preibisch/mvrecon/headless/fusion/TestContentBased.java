56e416ea90a47a4358a2c4f36bb5f335b5bd539d
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-08-15 15:36:08 +0200

    added simple test of content-based fusion

---
+++ src/main/java/net/preibisch/mvrecon/headless/fusion/TestContentBased.java
@@ -0,0 +1,76 @@
+/*-
+ * #%L
+ * Software for the reconstruction of multi-view microscopic acquisitions
+ * like Selective Plane Illumination Microscopy (SPIM) Data.
+ * %%
+ * Copyright (C) 2012 - 2017 Multiview Reconstruction developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+package net.preibisch.mvrecon.headless.fusion;
+
+import java.io.File;
+
+import ij.ImageJ;
+import ij.ImagePlus;
+import mpicbg.spim.data.SpimDataException;
+import mpicbg.spim.io.IOFunctions;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.complex.ComplexFloatType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Util;
+import net.preibisch.mvrecon.process.export.DisplayImage;
+import net.preibisch.mvrecon.process.fusion.FusionTools;
+import net.preibisch.mvrecon.process.fusion.transformed.weights.ContentBasedRealRandomAccessible;
+
+public class TestContentBased
+{
+	public static void main( String[] args ) throws SpimDataException
+	{
+		new ImageJ();
+
+		final Img< FloatType > img = IOFunctions.openAs32BitArrayImg( new File( "/Volumes/home/Data/brain/fused_tp_0_ch_0_correct4.tif" ) );
+		//final ImagePlus imp = DisplayImage.getImagePlusInstance( img, true, "brain", Double.NaN, Double.NaN );
+		//imp.show();
+
+		testContentBased( img );
+	}
+
+	public static void testContentBased( final Img< FloatType > img )
+	{
+		final double[] sigma1 = Util.getArrayFromValue( FusionTools.defaultContentBasedSigma1, 3 );
+		final double[] sigma2 = Util.getArrayFromValue( FusionTools.defaultContentBasedSigma2 / 10, 3 );
+
+		final double scaling = 2.0;
+
+		sigma1[ 0 ] /= scaling;
+		sigma2[ 0 ] /= scaling;
+		sigma1[ 1 ] /= scaling;
+		sigma2[ 1 ] /= scaling;
+
+		sigma1[ 2 ] /= scaling * 5.0;
+		sigma2[ 2 ] /= scaling * 5.0;
+
+		IOFunctions.println( "Computing ... " + Util.printCoordinates( sigma1 ) + ", " + Util.printCoordinates( sigma2 ) );
+
+		ContentBasedRealRandomAccessible< FloatType > cb = new ContentBasedRealRandomAccessible< FloatType >( img, img.factory().imgFactory( new ComplexFloatType() ), sigma1, sigma2 );
+
+		IOFunctions.println( "Done ... " );
+
+		final ImagePlus imp = DisplayImage.getImagePlusInstance( cb.getContentBasedImg(), true, "brain", Double.NaN, Double.NaN );
+		imp.show();
+	}
+}
