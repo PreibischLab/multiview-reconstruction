46601445d93076fde3587dbecf98a212e819deb5
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-09-03 16:17:28 +0200

    can save & load all types of virtual blockers

--- src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/splitting/XmlIoSplitMultiResolutionImgLoader.java
+++ src/main/java/net/preibisch/mvrecon/fiji/spimdata/imgloaders/splitting/XmlIoSplitMultiResolutionImgLoader.java
@@ -32,35 +32,22 @@ import java.util.HashMap;
 
 import org.jdom2.Element;
 
-import bdv.ViewerImgLoader;
 import mpicbg.spim.data.SpimDataIOException;
-import mpicbg.spim.data.XmlHelpers;
 import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
-import mpicbg.spim.data.generic.sequence.BasicImgLoader;
 import mpicbg.spim.data.generic.sequence.ImgLoaderIo;
 import mpicbg.spim.data.generic.sequence.ImgLoaders;
 import mpicbg.spim.data.generic.sequence.XmlIoBasicImgLoader;
+import mpicbg.spim.data.sequence.MultiResolutionImgLoader;
 import mpicbg.spim.data.sequence.SequenceDescription;
 import mpicbg.spim.data.sequence.XmlIoSequenceDescription;
 import mpicbg.spim.io.IOFunctions;
-import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
 
-@ImgLoaderIo( format = "split.viewerimgloader", type = SplitViewerImgLoader.class )
-public class XmlIoSplitViewerImgLoader implements XmlIoBasicImgLoader< SplitViewerImgLoader >
+@ImgLoaderIo( format = "split.multiresolutionimgloader", type = SplitMultiResolutionImgLoader.class )
+public class XmlIoSplitMultiResolutionImgLoader implements XmlIoBasicImgLoader< SplitMultiResolutionImgLoader >
 {
-	public static final String SETUPIDS_NAME = "SetupIds";
-	public static final String SETUPIDS_TAG = "SetupIdDefinition";
-	public static final String SETUPIDS_NEW = "NewId";
-	public static final String SETUPIDS_OLD = "OldId";
-
-	public static final String INTERVAL_TAG_MIN = "min";
-	public static final String INTERVAL_TAG_MAX = "max";
-
-	public static final String OLDSETUP_NAME = "OldViewSetup";
-
 	@Override
-	public Element toXml( final SplitViewerImgLoader imgLoader, final File basePath ) 
+	public Element toXml( final SplitMultiResolutionImgLoader imgLoader, final File basePath ) 
 	{
 		final Element elem = new Element( "ImageLoader" );
 		elem.setAttribute( IMGLOADER_FORMAT_ATTRIBUTE_NAME, this.getClass().getAnnotation( ImgLoaderIo.class ).format() );
@@ -68,7 +55,7 @@ public class XmlIoSplitViewerImgLoader implements XmlIoBasicImgLoader< SplitView
 		try
 		{
 			final XmlIoBasicImgLoader< ? > imgLoaderIo = ImgLoaders.createXmlIoForImgLoaderClass( imgLoader.underlyingImgLoader.getClass() );
-			elem.addContent( createImgLoaderElement( imgLoaderIo, imgLoader.underlyingImgLoader, basePath ) );
+			elem.addContent( XmlIoSplitViewerImgLoader.createImgLoaderElement( imgLoaderIo, imgLoader.underlyingImgLoader, basePath ) );
 		}
 		catch( Exception e )
 		{
@@ -91,64 +78,25 @@ public class XmlIoSplitViewerImgLoader implements XmlIoBasicImgLoader< SplitView
 		final ArrayList< Integer > newSetupIds = new ArrayList<>( imgLoader.new2oldSetupId.keySet() );
 		Collections.sort( newSetupIds );
 
-		final Element setupIds = new Element( SETUPIDS_NAME );
+		final Element setupIds = new Element( XmlIoSplitViewerImgLoader.SETUPIDS_NAME );
 		for ( final int newSetupId : newSetupIds )
-			setupIds.addContent( setupToXml( newSetupId, imgLoader.new2oldSetupId.get( newSetupId ), imgLoader.newSetupId2Interval.get( newSetupId ) ) );
+			setupIds.addContent( XmlIoSplitViewerImgLoader.setupToXml( newSetupId, imgLoader.new2oldSetupId.get( newSetupId ), imgLoader.newSetupId2Interval.get( newSetupId ) ) );
 
 		elem.addContent( setupIds );
 
 		return elem;
 	}
 
-	protected static Element setupToXml( final int newSetupId, final int oldSetupId, final Interval interval )
-	{
-		final Element elem = new Element( SETUPIDS_TAG );
-
-		elem.addContent( XmlHelpers.intElement( SETUPIDS_NEW, newSetupId ) );
-		elem.addContent( XmlHelpers.intElement( SETUPIDS_OLD, oldSetupId ) );
-
-		final long[] min = new long[ interval.numDimensions() ];
-		final long[] max = new long[ interval.numDimensions() ];
-
-		interval.min( min );
-		interval.max( max );
-
-		elem.addContent( XmlHelpers.longArrayElement( INTERVAL_TAG_MIN, min ) );
-		elem.addContent( XmlHelpers.longArrayElement( INTERVAL_TAG_MAX, max ) );
-		
-		return elem;
-	}
-
-	protected static void setupFromXML( final Element setup, final HashMap< Integer, Integer > new2oldSetupId, final HashMap< Integer, Interval > newSetupId2Interval )
-	{
-		final int newSetupId = XmlHelpers.getInt( setup, SETUPIDS_NEW );
-		final int oldSetupId = XmlHelpers.getInt( setup, SETUPIDS_OLD );
-		final long[] min = XmlHelpers.getLongArray( setup, INTERVAL_TAG_MIN );
-		final long[] max = XmlHelpers.getLongArray( setup, INTERVAL_TAG_MAX );
-
-		new2oldSetupId.put( newSetupId, oldSetupId );
-		newSetupId2Interval.put( newSetupId, new FinalInterval( min, max ) );
-	}
-
-	/**
-	 * Casting madness.
-	 */
-	@SuppressWarnings( "unchecked" )
-	protected static < L extends BasicImgLoader > Element createImgLoaderElement( final XmlIoBasicImgLoader< L > imgLoaderIo, final BasicImgLoader imgLoader, final File basePath )
-	{
-		return imgLoaderIo.toXml( ( L ) imgLoader, basePath );
-	}
-
 	@Override
-	public SplitViewerImgLoader fromXml(
+	public SplitMultiResolutionImgLoader fromXml(
 			final Element elem, File basePath,
 			final AbstractSequenceDescription<?, ?, ?> sequenceDescription )
 	{
 		final HashMap< Integer, Integer > new2oldSetupId = new HashMap<>();
 		final HashMap< Integer, Interval > newSetupId2Interval = new HashMap<>();
 
-		for ( final Element setup : elem.getChild( SETUPIDS_NAME ).getChildren( SETUPIDS_TAG ) )
-			setupFromXML( setup, new2oldSetupId, newSetupId2Interval );
+		for ( final Element setup : elem.getChild( XmlIoSplitViewerImgLoader.SETUPIDS_NAME ).getChildren( XmlIoSplitViewerImgLoader.SETUPIDS_TAG ) )
+			XmlIoSplitViewerImgLoader.setupFromXML( setup, new2oldSetupId, newSetupId2Interval );
 
 		try
 		{
@@ -158,14 +106,14 @@ public class XmlIoSplitViewerImgLoader implements XmlIoBasicImgLoader< SplitView
 				throw new SpimDataIOException( "no <" + xmlIoSequenceDescription.getTag() + "> element found." );
 			final SequenceDescription oldSD = xmlIoSequenceDescription.fromXml( sdElem, basePath );
 
-			ViewerImgLoader underlyingImgLoader = null;
+			MultiResolutionImgLoader underlyingImgLoader = null;
 
 			final Element imgLoaderElem = elem.getChild( IMGLOADER_TAG );
 			final String format = imgLoaderElem.getAttributeValue( IMGLOADER_FORMAT_ATTRIBUTE_NAME );
 			final XmlIoBasicImgLoader< ? > imgLoaderIo = ImgLoaders.createXmlIoForFormat( format );
-			underlyingImgLoader = (ViewerImgLoader)imgLoaderIo.fromXml( imgLoaderElem, basePath, oldSD );
+			underlyingImgLoader = (MultiResolutionImgLoader)imgLoaderIo.fromXml( imgLoaderElem, basePath, oldSD );
 
-			return new SplitViewerImgLoader( underlyingImgLoader, new2oldSetupId, newSetupId2Interval, oldSD );
+			return new SplitMultiResolutionImgLoader( underlyingImgLoader, new2oldSetupId, newSetupId2Interval, oldSD );
 		}
 		catch( Exception e )
 		{
