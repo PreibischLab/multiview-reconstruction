6de1511e528f930604edb125193793954b86fea1
Marwan <zouinkhi.marwan@gmail.com>
2021-05-06 04:22:34 +0100

    working neuroglancer

--- src/main/java/net/preibisch/aws/gui/AWSLoadGui.java
+++ src/main/java/net/preibisch/aws/gui/AWSLoadGui.java
@@ -1,8 +1,17 @@
 package net.preibisch.aws.gui;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3URI;
+import com.bigdistributor.aws.data.CredentialConfig;
+import com.bigdistributor.aws.data.CredentialSupplier;
 import com.bigdistributor.aws.dataexchange.aws.s3.func.auth.AWSCredentialInstance;
 import com.bigdistributor.aws.dataexchange.aws.s3.func.bucket.S3BucketInstance;
+import com.bigdistributor.aws.spimloader.AWSSpimLoader;
+import com.bigdistributor.aws.utils.AWS_DEFAULT;
 import fiji.util.gui.GenericDialogPlus;
 import net.preibisch.aws.spimloader.AWSLoadParseQueryXML;
 import net.preibisch.aws.tools.AWSDataParam;
@@ -10,10 +19,9 @@ import net.preibisch.aws.tools.TempFolder;
 
 
 public class AWSLoadGui {
-    private final static String defaultBucketName = "bigstitcher";
+    private final static String defaultUri = "s3://mzouink-test/dataset-n5.xml";
     private final static String defaultKeyPath = "/Users/Marwan/Desktop/BigDistributer/aws_credentials/bigdistributer.csv";
-    private final static String defaultPath = "data";
-    private final static String defaultFileName = "dataset-n5.xml";
+
     private final static String defaultExtra = "interestpoints";
     private final static String defaultRegion = "eu-central-1";
     private AWSLoadParseQueryXML result;
@@ -23,10 +31,9 @@ public class AWSLoadGui {
         final GenericDialogPlus gd = new GenericDialogPlus("AWS Input");
         gd.addFileField("Key: ", defaultKeyPath, 45);
         gd.addMessage("");
-        gd.addStringField("Input uri: ", defaultBucketName, 30);
-        gd.addStringField("Path: ", defaultPath, 30);
-        gd.addStringField("XML File: ", defaultFileName, 30);
-        gd.addStringField("Extras: ", defaultExtra, 30);
+        gd.addStringField("Input uri: ", defaultUri, 30);
+        gd.addMessage("");
+//        gd.addStringField("Extras: ", defaultExtra, 30);
         gd.addStringField("Region: ", defaultRegion, 30);
 
         gd.showDialog();
@@ -35,26 +42,26 @@ public class AWSLoadGui {
             return false;
 
         String keyPath = gd.getNextString();
-        String bucketName = gd.getNextString();
-        String path = gd.getNextString();
-        String xmlFile = gd.getNextString();
-        String extrasField = gd.getNextString();
+        String uri = gd.getNextString();
+//        String extrasField = gd.getNextString();
         String regionField = gd.getNextString();
 
-        AWSLoadParseQueryXML result = null;
-        AWSCredentialInstance.init(keyPath);
-        S3BucketInstance.init(AWSCredentialInstance.get(), Regions.fromName(regionField),bucketName,path);
-        String[] extras = new String[]{};
-        if(!extrasField.isEmpty()) extras = extrasField.split(",");
+//        String[] extras = new String[]{};
+//        if(!extrasField.isEmpty()) extras = extrasField.split(",");
 
-        AWSDataParam params = AWSDataParam.init(bucketName, path, xmlFile, Regions.fromName(regionField), extras);
+        AWSCredentials credentials = AWSCredentialInstance.init(keyPath).get();
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.fromName(regionField))
+                .build();
+
+        AWSSpimLoader.init(s3client, uri);
+        CredentialConfig.set(new CredentialSupplier(credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey(),regionField));
+//        AWSDataParam params = AWSDataParam.init(bucketName, path, xmlFile, Regions.fromName(regionField), extras);
            result = new AWSLoadParseQueryXML();
 
-        try {
-            if (!result.queryXML(S3BucketInstance.get(), TempFolder.get(),params)) {
-                return false;
-            }
-        } catch (IllegalAccessException e) {
+        if (!result.queryXML(s3client, TempFolder.get(),new AmazonS3URI(uri))) {
             return false;
         }
         AWSDataParam.setCloudMode(true);
--- src/main/java/net/preibisch/aws/spimloader/AWSLoadParseQueryXML.java
+++ src/main/java/net/preibisch/aws/spimloader/AWSLoadParseQueryXML.java
@@ -1,6 +1,9 @@
 package net.preibisch.aws.spimloader;
 
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3URI;
+import com.bigdistributor.aws.dataexchange.aws.s3.S3Utils;
 import com.bigdistributor.aws.dataexchange.aws.s3.func.bucket.S3BucketInstance;
 import mpicbg.spim.data.SpimDataException;
 import net.preibisch.aws.tools.AWSDataParam;
@@ -12,13 +15,14 @@ import java.io.IOException;
 
 public class AWSLoadParseQueryXML extends LoadParseQueryXML {
 
-    public boolean queryXML(S3BucketInstance s3, File tmpFolder, AWSDataParam params) {
+    public boolean queryXML(AmazonS3 s3, File tmpFolder, AmazonS3URI uri) {
         try {
-            this.xmlfilename = params.getXmlFile();
+            this.xmlfilename = uri.getKey();
             this.io = new XmlIoSpimData2("");
-            this.data = io.load(s3.download(tmpFolder, params.getXmlFile(), params.getPath()).getAbsolutePath());
-            s3.downloadFrom(tmpFolder, params.getPath(), params.getExtraFiles());
-        } catch (SpimDataException | InterruptedException | IOException e) {
+
+            this.data = io.load(S3Utils.download(s3,tmpFolder,uri.getURI().toString()).getAbsolutePath());
+//            s3.downloadFrom(tmpFolder, params.getPath(), params.getExtraFiles());
+        } catch (SpimDataException e) {
             e.printStackTrace();
             return false;
         }
