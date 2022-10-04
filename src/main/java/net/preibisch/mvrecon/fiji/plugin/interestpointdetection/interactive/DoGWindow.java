fa6ae15929b0a9ed4aaafcf5142c913fd29a3b14
Stephan Preibisch <stephan.preibisch@gmx.de>
2022-10-04 13:58:05 -0400

    added listener for min/max that update parameter object

--- src/main/java/net/preibisch/mvrecon/fiji/plugin/interestpointdetection/interactive/DoGWindow.java
+++ src/main/java/net/preibisch/mvrecon/fiji/plugin/interestpointdetection/interactive/DoGWindow.java
@@ -101,6 +101,8 @@ public class DoGWindow
 		/* Configuration */
 		sigma1Bar.addAdjustmentListener(new SigmaListener(parent,sigmaText1, InteractiveRadialSymmetry.sigmaMin, InteractiveRadialSymmetry.sigmaMax, parent.scrollbarSize, sigma1Bar));
 		thresholdBar.addAdjustmentListener(new ThresholdListener(parent,thresholdText, InteractiveRadialSymmetry.thresholdMin, InteractiveRadialSymmetry.thresholdMax));
+		minima.addItemListener( l -> parent.params.findMaxima = maxima.getState() );
+		minima.addItemListener( l -> parent.params.findMinima = minima.getState() );
 		button.addActionListener(new FinishedButtonListener(parent, false));
 		cancel.addActionListener(new FinishedButtonListener(parent, true));
 		doGFrame.addWindowListener(new FrameListener(parent));
