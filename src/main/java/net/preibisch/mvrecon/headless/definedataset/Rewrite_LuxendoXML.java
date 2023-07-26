ef2dbeae796234be2b65c42fa1b8bb49e189364c
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-07-26 11:15:43 -0400

    added code to run rewrite of Luxendo XML as Fiji plugin

--- src/main/java/net/preibisch/mvrecon/headless/definedataset/Rewrite_LuxendoXML.java
+++ src/main/java/net/preibisch/mvrecon/headless/definedataset/Rewrite_LuxendoXML.java
@@ -1,9 +1,12 @@
 package net.preibisch.mvrecon.headless.definedataset;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import fiji.util.gui.GenericDialogPlus;
+import ij.plugin.PlugIn;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.XmlIoSpimData;
 import mpicbg.spim.data.sequence.Angle;
@@ -14,17 +17,19 @@ import mpicbg.spim.data.sequence.Tile;
 import mpicbg.spim.data.sequence.ViewDescription;
 import net.imglib2.util.Pair;
 import net.imglib2.util.ValuePair;
+import net.preibisch.legacy.io.IOFunctions;
+import net.preibisch.mvrecon.fiji.plugin.util.GUIHelper;
 import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
 import net.preibisch.mvrecon.fiji.spimdata.XmlIoSpimData2;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
-public class RewriteLuxendoXML implements Callable<Void> 
+public class Rewrite_LuxendoXML implements Callable<Void>, PlugIn
 {
 	@Option(names = {"-i", "--input"}, required = true, description = "input XML written by Luxendo MuviSPIM, e.g. -i /home/dataset.xml")
 	private String input = null;
 
-	@Option(names = {"-o", "--output"}, required = false, description = "output XML for the same Luxendo MuviSPIM containing BigStitcher attributes, e.g. -i /home/dataset_bigstitcher.xml")
+	@Option(names = {"-o", "--output"}, required = false, description = "optional output file path for the Luxendo MuviSPIM XML containing BigStitcher attributes (input is overwritten if left empty, .xml~1 is backup created), e.g. -i /home/dataset_bigstitcher.xml")
 	private String output = null;
 
 	// e.g. stack_0-x00-y00_channel_1_obj_right_cam_long
@@ -34,16 +39,24 @@ public class RewriteLuxendoXML implements Callable<Void>
 	final static String searchCh = "_channel_";
 	final static String searchObj = "_obj_";
 
+	public static String defaultInput = "";
+
 	@Override
 	public Void call() throws Exception
 	{
+		if ( !new File( input ).exists() )
+		{
+			IOFunctions.println( "XML file '" + new File( input ).getAbsolutePath() + "' does not exist. Stopping.");
+			return null;
+		}
+
 		// angle and illumination are constant (for now)
 		final Angle angle = new Angle( 0 );
 		final Illumination illum = new Illumination( 0 );
 
 		try
 		{
-			System.out.println( "Loading '" + input + "' ... ");
+			IOFunctions.println( "Loading '" + input + "' ... ");
 
 			final XmlIoSpimData io = new XmlIoSpimData();
 			final SpimData spimData = io.load( input );
@@ -58,12 +71,12 @@ public class RewriteLuxendoXML implements Callable<Void>
 			{
 				final String name = vd.getViewSetup().getName();
 
-				System.out.print( "Parsing ViewSetup: " + name + " ... " );
+				IOFunctions.println( "Parsing ViewSetup: " + name + " ... " );
 
 				final int[] p = parseName(name); //x,y,ch
 				tileIds.computeIfAbsent( new ValuePair<>(p[0], p[1]), k -> countTileId.getAndIncrement() );
 
-				System.out.println( "x=" + p[0] + ", y=" + p[1] + ", ch=" + p[2] );
+				IOFunctions.println( "x=" + p[0] + ", y=" + p[1] + ", ch=" + p[2] );
 			}
 
 			for ( final ViewDescription vd : s.getViewDescriptions().values() )
@@ -74,7 +87,7 @@ public class RewriteLuxendoXML implements Callable<Void>
 				final int tileId = tileIds.get( new ValuePair<>(p[0], p[1]) );
 				final int channelId = p[2];
 
-				System.out.println( "Adding Attributes to ViewSetup: " + name +": x=" + p[0] + ", y=" + p[1] + ", tileId=" + tileId + ", ch=" + p[2] + ", channelId=" + channelId );
+				IOFunctions.println( "Adding Attributes to ViewSetup: " + name +": x=" + p[0] + ", y=" + p[1] + ", tileId=" + tileId + ", ch=" + p[2] + ", channelId=" + channelId );
 
 				// set missing attributes
 				vd.getViewSetup().setAttribute( angle );
@@ -83,13 +96,16 @@ public class RewriteLuxendoXML implements Callable<Void>
 				vd.getViewSetup().setAttribute( new Tile(tileId, "x=" + p[0] + ", y=" + p[1] ) );
 			}
 
-			System.out.println( "Saving '" + output + "' ... ");
+			output = (output == null || output.trim().length() == 0 ) ? input : output;
+
+			IOFunctions.println( "Saving '" + output + "' ... ");
 
 			XmlIoSpimData2.initN5Writing = false;
+
 			final XmlIoSpimData2 io2 = new XmlIoSpimData2( "" );
 			io2.save(spimDataOut, output);
 
-			System.out.println( "done.");
+			IOFunctions.println( "done.");
 		}
 		catch ( Exception e )
 		{
@@ -118,7 +134,35 @@ public class RewriteLuxendoXML implements Callable<Void>
 		return new int[] {x,y,ch};
 	}
 
-	public static final void main(final String... args) {
-		new CommandLine( new RewriteLuxendoXML() ).execute( args );
+	public static final void main(final String... args)
+	{
+		IOFunctions.printIJLog = false;
+		new CommandLine( new Rewrite_LuxendoXML() ).execute( args );
+	}
+
+	@Override
+	public void run(String arg)
+	{
+		GenericDialogPlus gd = new GenericDialogPlus( "Rewrite Luxendo XML for BigStitcher" );
+		gd.addFileField( "Input_XML", defaultInput );
+		gd.addMessage(
+				"Note: the new XML will contain the attributes necessary to run BigStitcher\n" +
+				"(multi-tile, multi-channel is supported for now, please contact Stephan Preibisch if you have other examples)", GUIHelper.smallStatusFont, GUIHelper.neutral );
+		gd.addMessage("Note: XML will be overwritten, a backup is stored as '*.xml~1'", GUIHelper.smallStatusFont, GUIHelper.neutral );
+
+		gd.showDialog();
+
+		if (gd.wasCanceled())
+			return;
+
+		this.input = defaultInput = gd.getNextString();
+		this.output = null;
+
+		IOFunctions.printIJLog = true;
+
+		try
+		{
+			call();
+		} catch (Exception e) { e.printStackTrace(); }
 	}
 }
