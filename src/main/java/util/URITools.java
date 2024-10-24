c5d1392777e0c42f30f3987671af16f1e178f69b
Stephan Preibisch <stephan.preibisch@gmx.de>
2024-10-24 16:18:00 +0200

    workaround for missing "/" at the end of the basepath for the XML @tpietzsch

--- src/main/java/util/URITools.java
+++ src/main/java/util/URITools.java
@@ -91,10 +91,41 @@ public class URITools
 		}
 	}
 
+	public static URI getParentURI( final URI uri ) throws SpimDataIOException
+	{
+		try
+		{
+			final String uriPath = uri.getPath();
+			final int parentSlash = uriPath.lastIndexOf( "/", uriPath.length() - 2 );
+			if ( parentSlash < 0 )
+			{
+				throw new SpimDataIOException( "URI is already at the root" );
+			}
+			// NB: The "+ 1" below is *very important*, so that the resultant URI
+			// ends in a trailing slash. The behaviour of URI differs depending on
+			// whether this trailing slash is present; specifically:
+			//
+			// * new URI("file:/foo/bar/").resolve(".") -> "file:/foo/bar/"
+			// * new URI("file:/foo/bar").resolve(".") -> "file:/foo/"
+			//
+			// That is: /foo/bar/ is considered to be in the directory /foo/bar,
+			// whereas /foo/bar is considered to be in the directory /foo.
+			final String parentPath = uriPath.substring( 0, parentSlash + 1 );
+			return new URI( uri.getScheme(), uri.getUserInfo(), uri.getHost(),
+				uri.getPort(), parentPath, uri.getQuery(), uri.getFragment() );
+		}
+		catch ( URISyntaxException e )
+		{
+			throw new SpimDataIOException( e );
+		}
+	}
+
 	public static void saveSpimData( final SpimData2 data, final URI xmlURI, final XmlIoSpimData2 io ) throws SpimDataException
 	{
 		if ( URITools.isFile( xmlURI ) )
 		{
+			data.setBasePathURI( getParentURI( xmlURI ) );
+
 			// old code for filesystem
 			io.save( data, URITools.fromURI( xmlURI ) );
 		}
