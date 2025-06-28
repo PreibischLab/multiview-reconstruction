8845774be1878fff58ffcf8ea9492bb29684a733
Stephan Preibisch <stephan.preibisch@gmx.de>
2025-06-27 21:56:36 -0400

    update licenses

--- src/main/java/net/preibisch/mvrecon/process/fusion/intensity/mpicbg/RansacRegressionReduceFilter.java
+++ src/main/java/net/preibisch/mvrecon/process/fusion/intensity/mpicbg/RansacRegressionReduceFilter.java
@@ -1,22 +1,23 @@
 /*-
  * #%L
- * TrakEM2 plugin for ImageJ.
+ * Software for the reconstruction of multi-view microscopic acquisitions
+ * like Selective Plane Illumination Microscopy (SPIM) Data.
  * %%
- * Copyright (C) 2005 - 2021 Albert Cardona, Stephan Saalfeld and others.
+ * Copyright (C) 2012 - 2025 Multiview Reconstruction developers.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
+ * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 package net.preibisch.mvrecon.process.fusion.intensity.mpicbg;
